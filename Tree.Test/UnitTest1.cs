d10f7160fd3c124dcfabfdf3d85b37c549117d22
kevinj <kevin@rocksolidknowledge.com>
2014-10-07 09:03:38 -0700

    Initial Tree code with MiniMax

---
+++ Tree.Test/UnitTest1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Tree.Test
+{
+    [TestFixture]
+    public class Tree
+    {
+        private Mock<IEvaluator> _evaluator;
+        private readonly Func<int> _simpleChildCalc = () => 3;
+
+        [SetUp]
+        public void Setup()
+        {
+            _evaluator = new Mock<IEvaluator>();
+        }
+
+        [Test]
+        public void ShouldHaveChildren()
+        {
+            var root = new MaxNode(_evaluator.Object, _simpleChildCalc);
+
+            root.Add(new MinNode(root));
+
+            Assert.AreEqual(1, root.Children.Count);
+        }
+
+        [Test]
+        public void ShouldBuildTreeWithThreeChildren()
+        {
+            var root = new MaxNode(_evaluator.Object, _simpleChildCalc);
+
+            root.FindBestMove(3);
+            root.Children.Should().NotBeNull();
+            var firstChild = root.Children[0];
+            var secondChild = firstChild.Children[0];
+            var thirdChild = secondChild.Children[0];
+            thirdChild.Should().NotBeNull();
+        }
+
+        [Test]
+        public void ShouldBuildTreeWithNoMoreThanThreeChildren()
+        {
+            var root = new MaxNode(_evaluator.Object, _simpleChildCalc);
+
+            root.FindBestMove(3);
+            root.Children.Should().NotBeNull();
+            var firstChild = root.Children[0];
+            var secondChild = firstChild.Children[0];
+            var thirdChild = secondChild.Children[0];
+            thirdChild.Children.Should().BeEmpty();
+        }
+
+        [Test]
+        public void ShouldCreateTheCorrectNumberOfChildren()
+        {
+            var root = new MaxNode(new SimpleEvaluator(), _simpleChildCalc, 1);
+
+            root.FindBestMove(1);
+
+            root.Children.Count.Should().Be(3);
+        }
+
+        [Test]
+        public void ShouldHaveChildrenWithTheCorrectScores()
+        {
+            var root = new MaxNode(new SimpleEvaluator(), _simpleChildCalc, 1);
+
+            root.FindBestMove(1);
+
+            int count = 0;
+            root.Children.ForEach(child => child.Score.Should().Be(count++));
+        }
+
+        [Test]
+        public void ShouldHaveTheCorrectRootMinMaxValueWithOnePly()
+        {
+            int depth = 1;
+            var root = new MaxNode(new SimpleEvaluator(), _simpleChildCalc, depth);
+
+            root.FindBestMove(depth);
+
+            root.Score.Should().Be(2);
+        }
+
+        [Test]
+        public void ShouldHaveTheCorrectRootMinMaxValueWithTwoPly()
+        {
+            int depth = 2;
+            var root = new MaxNode(new SimpleEvaluator(), _simpleChildCalc, depth);
+
+            root.FindBestMove(depth);
+
+            root.Score.Should().Be(6);
+        }
+    }
+
+    class SimpleEvaluator : IEvaluator
+    {
+        private int _count;
+        public int Evaluate()
+        {
+            return _count++;
+        }
+    }
+}
---
+++ Tree/Node.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+
+namespace Tree
+{
+    public interface IEvaluator
+    {
+        int Evaluate();
+    }
+    public class ChessEvaluator : IEvaluator
+    {
+        public int Evaluate()
+        {
+            return 1;
+        }
+    }
+
+    public abstract class Node
+    {
+        public Node(IEvaluator evaluator, Func<int> childMoveCalc, int depth = 3) : this()
+        {
+            Evaluator = evaluator;
+            MaxDepth = depth;
+        }
+
+        protected Node()
+        {
+            Children = new List<Node>();            
+        }
+
+        public int MaxDepth { get; set; } //?
+        public List<Node> Children { get; private set; }
+
+        public void Add(Node node)
+        {
+            Children.Add(node);
+        }
+
+        public int Score { get; set; }
+
+        public Node Parent { get; set; }
+        public Node Root { get; set; }
+
+        public void FindBestMove(int depth)
+        {
+            int nChildren = Root.CalculateNumberOfValidChildMoves();
+            if (depth > 0)
+            {
+                depth--;
+                while (nChildren-- > 0)
+                {
+                    var node = CreateChild();
+                    Children.Add(node);
+                    node.FindBestMove(depth);
+                }
+                Evaluate();                
+            }
+            else
+            {
+                Evaluate();                
+            }
+            
+        }
+
+        protected abstract Node CreateChild();
+
+        public Func<int> CalculateNumberOfValidChildMoves = () => 3;
+
+        public virtual void Evaluate()
+        {
+            if (Children.Count == 0)
+            {
+                Score = Root.Evaluator.Evaluate();
+            }
+            else
+            {
+                Score = GetMiniMaxScore();
+            }
+        }
+
+        protected abstract int GetMiniMaxScore();
+
+        protected IEvaluator Evaluator;
+    }
+
+    public class MinNode : Node
+    {
+        public MinNode(Node root)
+        {
+            Root = root;
+            Score = int.MinValue;
+        }
+
+        protected override Node CreateChild()
+        {
+            var node = new MaxNode(Root);
+            return node;
+        }
+
+        protected override int GetMiniMaxScore()
+        {
+            int minimum = int.MaxValue;
+            Children.ForEach(child =>
+            {
+                minimum = child.Score < minimum ? child.Score : minimum;
+            });
+            return minimum;
+        }
+    }
+    public class MaxNode : Node
+    {
+        public MaxNode(IEvaluator evaluator, Func<int> childMoveCalc, int depth = 3)
+            : base(evaluator, childMoveCalc, depth)
+        {
+            Root = this;
+            Score = int.MaxValue;
+        }
+
+        internal MaxNode(Node root)
+        {
+            Root = root;
+        }
+
+
+        protected override Node CreateChild()
+        {
+            var node = new MinNode(Root);
+            return node;
+        }
+
+        protected override int GetMiniMaxScore()
+        {
+            int maximum = int.MinValue;
+            Children.ForEach(child =>
+            {
+                maximum = child.Score > maximum ? child.Score : maximum;
+            });
+            return maximum;
+        }
+    }
+}
